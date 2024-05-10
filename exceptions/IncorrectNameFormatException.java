8ea53759d36a0c0f8c6e08d61895c7bccb40e75c
MIKE <kpocobok.muxa@mail.ru>
2024-05-10 22:38:44 +0300

    1

---
+++ exceptions/IncorrectNameFormatException.java
@@ -0,0 +1,7 @@
+package org.example.exceptions;
+
+public class IncorrectNameFormatException extends Throwable {
+    public IncorrectNameFormatException() {
+        super("you entered an incorrect expression for your full name");
+    }
+}
